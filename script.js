9e0f73a39e013532172151c700c1cabfae776900
Axton Smith <axtonsmith@gmail.com>
2022-01-08 17:37:33 -0500

    added script

---
+++ script.js
@@ -0,0 +1,38 @@
+const button = document.getElementById('button')
+button.onclick = () => rollDice()
+
+function rollDice() {
+    let numOfDice = prompt("How many blue dice do you have?");
+    let numOfRolls = prompt("How many combat rounds in a row do you want to calculate?");
+
+    let death = 0;
+    let life = 0;
+    for (let i = 0; i < numOfRolls; i++) {
+        let ones = 0;
+        let eights = 0;
+        for (let i = 0; i < numOfDice;) {
+            let roll = Math.floor(Math.random() * 8);
+            if (roll == 0) {
+                ones++;
+                i++;
+            }
+            else if (roll == 7) {
+                eights++;
+                i++;
+            }
+            else {
+                i++;
+            }
+
+        }
+        if (ones > eights) {
+
+            death++;
+        } else {
+            life++;
+        }
+    }
+    let chance = (death / life * 100).toFixed(2);
+    alert(`Your brand new Ornate Blunderbuss, only 8 gold, incredible value wow has only an ${chance}% chance to explode 
+    if you roll ${numOfDice} blue dice over ${numOfRolls} combat rounds`);
+}
